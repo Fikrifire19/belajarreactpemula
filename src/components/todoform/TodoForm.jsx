dbc9fbd8e1d2c08f22c24e664f50a053394bf968
Muhamad Fikri Mubarok <40727135+Fikrifire19@users.noreply.github.com>
2020-08-23 21:53:42 +0700

    membuat modul css untuk komponen todoform

---
+++ src/components/todoform/TodoForm.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import styles from "./todoform.module.css";
+
+const TodoForm = ({ addTodo, showAdd }) => {
+  const [value, setValue] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      alert("No Blank Todo!");
+      return;
+    }
+
+    if (value.length > 40) {
+      alert("Teks Terlalu Panjang!");
+      setValue("");
+      return;
+    }
+
+    if (value.length < 5) {
+      alert("Teks Terlalu Pendek!");
+      setValue("");
+      return;
+    }
+
+    addTodo(value);
+    setValue("");
+  };
+
+  if (showAdd) {
+    return (
+      <section className={styles.add}>
+        <form className={styles.addForm} onSubmit={handleFormSubmit}>
+          <input
+            type="text"
+            className={styles.addInput}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <button className={styles.addBtn}>Add</button>
+        </form>
+      </section>
+    );
+  } else {
+    return null;
+  }
+};
+
+TodoForm.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  showAdd: PropTypes.bool.isRequired
+};
+
+export default TodoForm;
