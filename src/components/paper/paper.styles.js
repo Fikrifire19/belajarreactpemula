6a006fc3f3367766ce1a78f63e5b5ae4edee1c84
Muhamad Fikri Mubarok <40727135+Fikrifire19@users.noreply.github.com>
2020-08-25 08:30:37 +0700

    membuat emotion css props untuk komponen paper

---
+++ src/components/paper/paper.styles.js
@@ -0,0 +1,20 @@
+import { css } from "@emotion/core";
+
+export const paper = ({ theme }) =>
+  css`
+    width: 600px;
+    height: 900px;
+    background-color: ${theme.background.color.primary};
+    border-radius: 5px;
+    padding: 32px;
+  `;
+
+export const frame = ({ theme }) =>
+  css`
+    border: 1px solid ${theme.color.primary.black};
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  `;
