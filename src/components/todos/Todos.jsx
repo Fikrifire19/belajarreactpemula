3c19534f7f6b20fa64f33967870bacd7ad90890a
Muhamad Fikri Mubarok <40727135+Fikrifire19@users.noreply.github.com>
2020-08-25 23:33:38 +0700

    implementasi layout pada todos component

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -1,31 +1,38 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+//import React from "react";
 import PropTypes from "prop-types";
 
-import styles from "./todos.module.css";
+//import styles from "./todos.module.css";
+import * as styles from "./todos.styles";
 import Todo from "../todo/Todo";
+import Container from "../../layout/Container";
+//import Item from "../../layout/Item";
 
 const Todos = ({ todos, completeTodo }) => {
   return (
-    <section className={styles.todos}>
-      {todos.length > 0 &&
-        todos.map((todo, index) => {
-          return (
-            <Todo
-              key={index}
-              text={todo.text}
-              isCompleted={todo.isCompleted}
-              completeTodo={completeTodo}
-              index={index}
-            />
-          );
-        })}
-      {todos.length === 0 && (
-        <div className={styles.todoPlaceholderText}>
-          Tambahkan Todo dengan mengkilik tombol{" "}
-          <span className={styles.addBtnPlaceholderText}>Add</span> diatas
-          sebelah kiri
-        </div>
-      )}
+    <section className="todos-component">
+      <Container flexDirection="column" minHeight="500px">
+        {todos.length > 0 &&
+          todos.map((todo, index) => {
+            return (
+              <Todo
+                key={index}
+                text={todo.text}
+                isCompleted={todo.isCompleted}
+                completeTodo={completeTodo}
+                index={index}
+              />
+            );
+          })}
+        {todos.length === 0 && (
+          <div css={styles.todoPlaceholderText}>
+            Tambahkan Todo dengan mengkilik tombol{" "}
+            <span css={styles.addBtnPlaceholderText}>Add</span> diatas sebelah
+            kiri
+          </div>
+        )}
+      </Container>
     </section>
   );
 };
